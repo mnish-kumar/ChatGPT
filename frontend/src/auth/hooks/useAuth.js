31c9d55f7edd77b8a1c8e7121ed4edde9056169e
Manish Kumar <manishkr.smi@gmail.com>
2026-04-14 23:31:23 +0530

    Refactor useAuth hook for improved readability and consistency

--- frontend/src/auth/hooks/useAuth.js
+++ frontend/src/auth/hooks/useAuth.js
@@ -1,94 +1,94 @@
 import { useContext, useState } from "react";
-import {AuthContext} from "../services/auth.context";
+import { AuthContext } from "../services/auth.context";
 import { login, register, logout } from "../API/auth.api";
 import { createChat, getMessages } from "../API/chat.api";
 
 export const useAuth = () => {
-    const context = useContext(AuthContext);
-    const {user, setUser, loading, setLoading} = context;
-    const [isSubmitting, setIsSubmitting] = useState(false);
+  const context = useContext(AuthContext);
+  const { user, setUser, loading, setLoading } = context;
+  const [isSubmitting, setIsSubmitting] = useState(false);
 
-    const handleLogin = async ({ email, password }) => {
-        setIsSubmitting(true);
-        setLoading(true);
+  const handleLogin = async ({ email, password }) => {
+    setIsSubmitting(true);
+    setLoading(true);
 
-        try {
-            const data = await login({ email, password });
-            setUser(data.user);
-            return { success: true };
-        } catch (error) {
-            console.error("Login failed:", error);
-            return { success: false, error };
-        } finally {
-            setIsSubmitting(false);
-            setLoading(false);
-        }
+    try {
+      const data = await login({ email, password });
+      setUser(data.user);
+      return { success: true };
+    } catch (error) {
+      console.error("Login failed:", error);
+      return { success: false, error };
+    } finally {
+      setIsSubmitting(false);
+      setLoading(false);
     }
+  };
 
-    const handleRegister = async ({ fullname, email, password }) => {
-        setIsSubmitting(true);
-        setLoading(true);
-        
-        try {
-            const data = await register({ fullname, email, password });
-            setUser(data.user);
-            return { success: true };
-        } catch (error) {
-            console.error("Register failed:", error);
-            return { success: false, error };
-        } finally {
-            setIsSubmitting(false);
-            setLoading(false);
-        }
+  const handleRegister = async ({ fullname, email, password }) => {
+    setIsSubmitting(true);
+    setLoading(true);
+
+    try {
+      const data = await register({ fullname, email, password });
+      setUser(data.user);
+      return { success: true };
+    } catch (error) {
+      console.error("Register failed:", error);
+      return { success: false, error };
+    } finally {
+      setIsSubmitting(false);
+      setLoading(false);
     }
+  };
 
-    const handleLogout = async () => {
-        setIsSubmitting(true);
+  const handleLogout = async () => {
+    setIsSubmitting(true);
 
-        try {
-            await logout();
-            setUser(null);
-        }catch (error) {
-            console.error("Logout failed:", error);
-        }finally {
-            setIsSubmitting(false);
-        }
+    try {
+      await logout();
+      setUser(null);
+    } catch (error) {
+      console.error("Logout failed:", error);
+    } finally {
+      setIsSubmitting(false);
     }
-    
-    const handleCreateChat = async ({ title }) => {
-        try {                
-            const data = await createChat({ title });
-            return data.chat;
-        } catch (error) {
-            console.error("Error creating chat:", error);
-            throw error;
-        } finally {
-            setIsSubmitting(false);
-        }
+  };
+
+  const handleCreateChat = async ({ title }) => {
+    try {
+      const data = await createChat({ title });
+      return data.chat;
+    } catch (error) {
+      console.error("Error creating chat:", error);
+      throw error;
+    } finally {
+      setIsSubmitting(false);
     }
+  };
 
-    const handleGetMessages = async (chatId) => {
-        setLoading(true);
-        try {
-            const data = await getMessages(chatId);
+  const handleGetMessages = async (chatId) => {
+    setLoading(true);
+    try {
+      const data = await getMessages(chatId);
 
-            return data.messages;
-        } catch (error) {
-            console.error("Error fetching messages:", error);
-            throw error;
-        } finally {
-            setLoading(false);
-        }
+      return data.messages;
+    } catch (error) {
+      console.error("Error fetching messages:", error);
+      throw error;
+    } finally {
+      setLoading(false);
     }
+  };
 
-    return {
-        user,
-        loading,
-        isSubmitting,
-        handleLogin,
-        handleRegister,
-        handleLogout,
-        handleCreateChat,
-        handleGetMessages,
-    };
-}
+  return {
+    user,
+    loading,
+    isSubmitting,
+    handleLogin,
+    handleRegister,
+    handleLogout,
+    handleCreateChat,
+    handleGetMessages,
+  };
+};
